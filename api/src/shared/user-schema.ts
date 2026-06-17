9fbf25e1325a5f686a6c0fef442a103985eac881
constElmar <elmargamalo@gmail.com>
2026-06-17 13:49:18 +0800

    update user schema

---
+++ api/src/shared/user-schema.ts
@@ -0,0 +1,12 @@
+import {z} from 'zod';
+
+const registerUserSchema = z.object({
+	name: z.string(),
+	email: z.email({error: "Invalid email address"}),
+	password: z.string().min(8,'Password too short')
+});
+
+type RegisterUser = z.infer<typeof registerUserSchema>;
+		
+
+export {registerUserSchema, RegisterUser}
